eb2f78ed4ad3375270999a0f070c749e92d63604
Chandni Upreti <chandniupreti5500@gmail.com>
2026-06-30 21:02:24 +0530

    final changes in server.js for deployment

--- backend/server.js
+++ backend/server.js
@@ -101,6 +101,8 @@ app.get("/test", (req, res) => {
   res.send("UPLOAD SERVER VERSION");
 });
 
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
